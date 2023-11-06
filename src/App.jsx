f0387e3d67d3b8a357cce1c2197c08a8bc59cff2
Mitul Joby <73733877+Mitul-Joby@users.noreply.github.com>
2023-11-07 04:55:05 +0530

    Rendering 3D face model with texture test

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import {OBJModel,DirectionLight} from 'react-3d-viewer'
+import {OBJModel,DirectionLight, MTLModel, Tick} from 'react-3d-viewer'
 
 
 function App() {
@@ -7,11 +7,24 @@ function App() {
 
   return (
     <>
-      <div style={{width:"100vw", display:"flex", justifyContent:"center"}}>
       <OBJModel src="./src/11.obj" width="400" height="400"  
         texPath=""/>
+      <DirectionLight color={0xffffff} position={{x:0,y:0,z:0}}/>
       
-    </div>
+      <MTLModel
+        enableZoom={false}
+        position={{x:0,y:0,z:0}}
+        rotation={{x:0,y:0,z:0}}
+        width={600}
+        height={600}
+        mtl='/src/assets/MitulFaceSmile.mtl'
+        src='/src/assets/MitulFaceSmile.obj'
+        texPath='/src/assets/'
+      />
+
+
+
+      
 
     </>
   )
